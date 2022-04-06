175a6be7accc0a028031369c51242ee830c2d24c
Koushal Kandagatla <96097275+k-Koushal@users.noreply.github.com>
2022-04-06 17:04:54 +0530

    Passenger counter bg changed

--- Passenger_couter_webapp/index.js
+++ Passenger_couter_webapp/index.js
@@ -6,7 +6,10 @@ function increment(){
     count+=1;
     countEl.innerText=count;
 }
-
+function decrement(){
+    count-=1;
+    countEl.innerText=count;
+}
 function save(){
     let countStr=count+" - "
     saveEl.textContent+=countStr;
